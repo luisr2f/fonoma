bc768335c0eec069d35416289111f746491084a0
Luis Ramon <luisr2f@gmail.com>
2023-09-24 19:31:18 -0400

    add page exchange

---
+++ pages/exchange/exchange.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+
+export default function Exchange({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Exchange</Text>     
+    </View>
+  );
+}
