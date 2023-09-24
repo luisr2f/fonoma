3ad263a6c68faf262364d5b5e91b7df19081b4ac
Luis Ramon <luisr2f@gmail.com>
2023-09-24 19:39:15 -0400

    layout page exchange

---
+++ _global/styleGlobal.tsx
@@ -0,0 +1,71 @@
+import {StyleSheet} from "react-native";
+
+import Colors from "./colors";
+
+export const styleGlobal = StyleSheet.create({
+	scrollView: {
+		padding: 12,
+		paddingVertical: 20,
+		backgroundColor: Colors.white
+	},
+	field: {
+		marginBottom: 20,
+	},
+	// button
+	btnCnt: {
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 20,
+	},
+	btn: {
+		height: 44,
+		borderRadius: 6,
+		borderWidth: 1,
+		borderColor: Colors.white,
+		backgroundColor: Colors.primary,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		paddingHorizontal: 20,
+	},
+	btnSubmit: {
+		minWidth: 220,
+	},
+
+	btnTxt: {
+		color: Colors.white,
+		fontSize: 14,
+		textTransform: "uppercase",
+		marginHorizontal: 12,
+	},
+	btnDisable: {backgroundColor: Colors.grayMin},
+	// END button
+
+	// textInput
+	textInputLabel: {color: Colors.gray, fontSize: 14, paddingBottom: 6, paddingLeft: 6},
+	textInputCntMain: {marginBottom: 24},
+	textInputCntMainError: {marginBottom: 6},
+	textInputCnt: {
+		alignSelf: "center",
+		flexDirection: "row",
+		paddingHorizontal: 6,
+		borderRadius: 6,
+		borderWidth: 1,
+		borderColor: Colors.gray,
+		backgroundColor: Colors.white,
+		height: 40,
+	},
+	textInputError: {borderColor: Colors.red, borderWidth: 1.5},
+	textInputTextError: {color: Colors.red, fontSize: 14, paddingBottom: 6, paddingTop: 2, paddingLeft: 6},
+	textInput: {
+		flex: 1,
+		paddingHorizontal: 8,
+		color: Colors.black,
+	},	
+	// END textInput
+
+	selectList: {
+		backgroundColor: Colors.white, borderRadius: 6, borderColor: Colors.gray
+	}
+});
--- pages/exchange/exchange.tsx
+++ pages/exchange/exchange.tsx
@@ -1,10 +1,71 @@
-import React from "react";
-import { Button, View, Text } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { Button, View, Text, TextInput, ScrollView } from "react-native";
+import { SelectList } from 'react-native-dropdown-select-list'
 
-export default function Exchange({ navigation }) {
+import { styleGlobal } from "../../_global/styleGlobal";
+import Colors from "../../_global/colors";
+
+export default function Exchange() {
+  const [amount, setAmount] = useState('');
+  const [from, setFrom] = useState('');
+  const [to, setTo] = useState('');
+
+  const data = [
+    { key: '1', value: 'Mobiles' },
+    { key: '2', value: 'Appliances' },
+    { key: '3', value: 'Cameras' },
+    { key: '4', value: 'Computers' },
+    { key: '5', value: 'Vegetables' },
+    { key: '6', value: 'Diary Products' },
+    { key: '7', value: 'Drinks' },
+  ]
+
+  const printSelect = (type: "from" | "to") => {
+    return (<>
+      <View style={styleGlobal.field}>
+        <Text style={styleGlobal.textInputLabel}>
+          {type === "from" ? "From" : "To"} *
+        </Text>
+        <SelectList
+          boxStyles={styleGlobal.selectList}
+          placeholder={"Select your currency ("+type+")"}
+          setSelected={(val) => type === "from" ? setFrom(val) : setTo(val)}
+          data={data}
+          //defaultOption={data[0]}
+          save="key"
+        />
+      </View>
+    </>)
+
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Exchange</Text>     
-    </View>
+    <ScrollView style={styleGlobal.scrollView} keyboardShouldPersistTaps="handled">
+
+      <View style={styleGlobal.field}>
+        <View style={[styleGlobal.textInputCntMain]}>
+          <Text style={styleGlobal.textInputLabel}>
+            Amount *
+          </Text>
+          <View style={[styleGlobal.textInputCnt]}>
+            <TextInput
+              keyboardType={"numeric"}
+              style={[styleGlobal.textInput]}
+              placeholder={'Insert amount'}
+              placeholderTextColor={Colors.gray}
+              onChangeText={text => {
+                setAmount(text);
+              }}
+              value={amount}
+            />
+          </View>
+        </View>
+
+        {printSelect("from")}
+
+        {printSelect("to")}
+
+      </View>
+    </ScrollView>
   );
 }
